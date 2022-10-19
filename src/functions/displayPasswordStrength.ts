6bef024b01b334cfa46cabcfee17ae71bffd9e61
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-10-19 21:46:05 +0200

    add displayPasswordStrength function and add place for its tests

---
+++ src/functions/displayPasswordStrength.ts
@@ -0,0 +1,59 @@
+enum PasswordStrengths {
+  TOO_WEAK = "too-weak",
+  WEAK = "weak",
+  MEDIUM = "medium",
+  STRONG = "strong",
+}
+
+const displayPasswordStrength = (
+  htmlDatasetName: string,
+  passwordStrength: PasswordStrengths
+): void => {
+  const templateSelector = getTemplateSelector(
+    htmlDatasetName,
+    passwordStrength
+  );
+  const templateElement = getTemplateElement(templateSelector);
+  const templateElementParent = getTemplateParent(templateElement);
+
+  removeNonTemplateElements(templateElementParent);
+
+  const templateContent = getTemplateContent(templateElement);
+  appendTemplateContent(templateElementParent, templateContent);
+};
+
+const getTemplateSelector = (
+  htmlDatasetName: string,
+  passwordStrength: string
+): string => {
+  return `[${htmlDatasetName}="${passwordStrength}"]`;
+};
+
+const getTemplateElement = (templateSelector: string): HTMLTemplateElement => {
+  return document.querySelector<HTMLTemplateElement>(templateSelector);
+};
+
+const getTemplateParent = (
+  templateElement: HTMLTemplateElement
+): ParentNode => {
+  return templateElement.parentNode;
+};
+
+const removeNonTemplateElements = (parentNode: ParentNode): void => {
+  parentNode.querySelectorAll("*:not(template)").forEach((e) => e.remove());
+};
+
+const getTemplateContent = (templateElement: HTMLTemplateElement): Node => {
+  return templateElement.content.cloneNode(true);
+};
+
+const appendTemplateContent = (
+  parentNode: ParentNode,
+  templateContent: Node
+): void => {
+  parentNode.appendChild(templateContent);
+};
+
+export default displayPasswordStrength;
+
+export { PasswordStrengths };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import setupRangeInputStyling from "./functions/setupRangeInputStyling";
+import displayPasswordStrength, {
+  PasswordStrengths,
+} from "./functions/displayPasswordStrength";
 import "./style.css";
 
 const rangeInputSelector = "[data-input-range]";
+const passwordStrengthHtmlDataset = "data-password-strength";
 
 setupRangeInputStyling(rangeInputSelector);
+displayPasswordStrength(passwordStrengthHtmlDataset, PasswordStrengths.MEDIUM);
