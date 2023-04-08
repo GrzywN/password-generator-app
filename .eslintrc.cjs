cf3a7a8f5a1f9548cf3771a282906ff74e8906dd
Karol Binkowski <starystachu9@gmail.com>
2023-04-08 17:20:33 +0200

    style: format files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,24 +4,24 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "standard-with-typescript",
-    "plugin:prettier/recommended",
-    "plugin:tailwindcss/recommended",
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
   ],
-  plugins: ["prettier", "tailwindcss"],
+  plugins: ['prettier', 'tailwindcss'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "./tsconfig.ts",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.ts',
   },
   rules: {
-    "prettier/prettier": [
-    "error",
-    {},
-    {
-      "usePrettierrc": true
-    }
-  ]
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
   },
-};
+}
