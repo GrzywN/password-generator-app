862763396d8c0946886e88453b1fac6a49c93f38
Karol Binkowski <starystachu9@gmail.com>
2023-04-08 17:23:24 +0200

    feat: add PubSub util for managing app state

---
+++ src/utils/pubsub/pubsub.ts
@@ -0,0 +1,23 @@
+export class PubSub {
+  private subscribers: Function[] = []
+
+  subscribe(fn: Function): void {
+    this.subscribers.push(fn)
+  }
+
+  unsubscribe(fn: Function): void {
+    this.subscribers = this.subscribers.filter(
+      (subscriber) => subscriber !== fn,
+    )
+  }
+
+  unsubscribeAll(): void {
+    this.subscribers = []
+  }
+
+  publish(data: unknown): void {
+    this.subscribers.forEach((subscriber) => subscriber(data))
+  }
+}
+
+export default PubSub
