2577371d2e3e27ec41900a841bf3f23810ba6f8a
Karol Binkowski <starystachu9@gmail.com>
2023-05-07 15:45:43 +0200

    feat: add Copy To Clipboard error state on empty password

--- src/libs/clipboard-util/clipboard-util.ts
+++ src/libs/clipboard-util/clipboard-util.ts
@@ -1,4 +1,8 @@
 export async function copyToClipboard(text: string): Promise<boolean> {
+  if (text.length === 0) {
+    return false;
+  }
+
   try {
     await navigator.clipboard.writeText(text);
   } catch (error) {
